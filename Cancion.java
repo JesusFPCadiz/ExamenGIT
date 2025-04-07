8b03c771fbc2359fa23cd89a44d6158708b8a767
JesusFPCadiz <jesusantonio.patricio.vico@iesfernandoaguilar.es>
2025-04-07 11:49:52 +0200

    Commit master

--- Cancion.java
+++ Cancion.java
@@ -2,7 +2,7 @@ package evf.javadoc.original;
 
 
  public class Cancion {
-    String nombreCancion;
+    String nombreCancionb;
     Artista artista;
 
     public Cancion(String nombreCancion, Artista artista) {
